d5e9e72c1362d7c28d45acc7c32684074f6442ed
Suman Bej <sumanbej.glt@gmail.com>
2026-01-08 20:36:52 +0530

    changed h2 config

--- user-service/src/main/java/com/paymentapp/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/com/paymentapp/user_service/config/SecurityConfig.java
@@ -13,7 +13,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll()
                 );
         return http.build();
---
+++ user-service/src/main/java/com/paymentapp/user_service/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.paymentapp.user_service.controller;
+
+import com.paymentapp.user_service.dto.UserRequest;
+import com.paymentapp.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest){
+        return userService.createUser(userRequest);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+}
