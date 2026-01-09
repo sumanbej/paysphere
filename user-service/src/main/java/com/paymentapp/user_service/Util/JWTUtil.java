e29546eaf0b10f4a9a4ca6599cb038c03a141794
Suman Bej <sumanbej.glt@gmail.com>
2026-01-09 20:30:50 +0530

    implementing jwt-1

--- user-service/src/main/java/com/paymentapp/user_service/Util/JWTUtil.java
+++ user-service/src/main/java/com/paymentapp/user_service/Util/JWTUtil.java
@@ -1,4 +1,37 @@
 package com.paymentapp.user_service.Util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Component
 public class JWTUtil {
+    private static final String SECRET="secret12345678";
+
+    private Key getSignInKey(){
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
 }
