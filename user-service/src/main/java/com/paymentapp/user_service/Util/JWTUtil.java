1da60c9fe0815b34fa3273ec80dfb48d4023e0b1
Suman Bej <sumanbej.glt@gmail.com>
2026-01-09 20:38:58 +0530

    implementing jwt-final

--- user-service/src/main/java/com/paymentapp/user_service/Util/JWTUtil.java
+++ user-service/src/main/java/com/paymentapp/user_service/Util/JWTUtil.java
@@ -2,19 +2,36 @@ package com.paymentapp.user_service.Util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Component
 public class JWTUtil {
-    private static final String SECRET="secret12345678";
+    private static final String SECRET="secret123secret123secret123secret123";
+    private static final long EXPIRATION_TIME = 86400000;
 
     private Key getSignInKey(){
         return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+    public String generateToken(Long userId, String email, String role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", userId);
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> resolver) {
@@ -32,6 +49,12 @@ public class JWTUtil {
 
     public String extractEmail(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+    public Long extractUserId(String token) {
+        return extractAllClaims(token).get("userId", Long.class);
     }
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
